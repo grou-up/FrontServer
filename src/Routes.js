1a6b620176e168f1c22bb71f8e9fa6ae8181e3c0
boseok <fa7271@naver.com>
2024-12-11 23:38:42 +0900

    add private route

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Routes로 수정
-import Login from './pages/Login';
-import SignupForm from "./components/SignupForm"; 
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import SignupForm from "./components/SignupForm";
 import MainForm from "./components/MainForm";
+import KakaoLoginCallback from "./pages/KakaoLoginCallback";
+import PrivateRoute from "./components/PrivateRoute"; // PrivateRoute 추가
+
 class AppRoutes extends React.Component {
     render() {
         return (
             <Router>
-                <Routes> {/* Switch 대신 Routes */}
+                <Routes>
+                    {/* 공개된 경로 */}
                     <Route path="/" element={<Login />} />
-                    {/* 회원가입 페이지 */}
                     <Route path="/signup" element={<SignupForm />} />
-                    {/* 메인 페이지 */}
-                    <Route path="/main" element={<MainForm />} />
+                    <Route path="/oauth/kakao/callback" element={<KakaoLoginCallback />} />
+
+                    {/* 보호된 경로 */}
+                    <Route
+                        path="/main"
+                        element={
+                            <PrivateRoute>
+                                <MainForm />
+                            </PrivateRoute>
+                        }
+                    />
                 </Routes>
             </Router>
         );
---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { getToken } from "../utils/tokenManager";
+
+const PrivateRoute = ({ children }) => {
+    // JWT 토큰을 localStorage에서 가져오기
+    const token = getToken()
+    console.log(token)
+    if (!token) {
+        // 토큰이 없으면 로그인 페이지로 리다이렉트
+        return <Navigate to="/" replace />;
+    }
+
+    // 인증된 경우 자식 컴포넌트를 렌더링
+    return children;
+};
+
+export default PrivateRoute;
