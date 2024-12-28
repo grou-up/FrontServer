710006e5aedf9f1eaf7173167a16ed6d6be13163
boseok <fa7271@naver.com>
2024-12-29 01:14:18 +0900

    fileUpload 화면 구현

--- src/services/pythonapi.js
+++ src/services/pythonapi.js
@@ -30,3 +30,17 @@ export const uploadFile2 = (file) => {
     },
   });
 };
+export const uploadFile3 = (file) => {
+  const formData = new FormData();
+  formData.append('file', file);
+
+  // 토큰 가져오기
+  const token = getToken();
+
+  // return axios.post('http://localhost:8000/upload_excel/', formData, {
+  //   headers: {
+  //     'Content-Type': 'multipart/form-data',
+  //     'Authorization': `Bearer ${token}`  // Authorization 헤더에 토큰 추가
+  //   },
+  // });
+};
