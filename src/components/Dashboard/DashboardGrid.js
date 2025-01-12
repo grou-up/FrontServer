2a39ab5342571a212a79def6002b20ef3955efdb
boseok <fa7271@naver.com>
2025-01-12 23:45:03 +0900

    totalSales 데이터 부모 통해 자식에게 전달

--- src/components/Dashboard/DashboardGrid.js
+++ src/components/Dashboard/DashboardGrid.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import SalesReport from "./SalesReport";
-import '../../styles/Dashboard.css';
-import TotalSalesReport from "./TotlaSalesReport";
+import "../../styles/Dashboard.css";
+import TotalSalesReport from "./TotalSalesReport";
+import TotalGraph from "./TotalGraph";
 
 const DashboardGrid = () => {
+    const [totalSalesData, setTotalSalesData] = useState([]);
+
     return (
         <div className="dashboard-grid">
             <div className="dashboard-item">
-                <TotalSalesReport />
+                <TotalSalesReport setTotalSalesData={setTotalSalesData} />
             </div>
             <div className="dashboard-item">
-                <SalesReport />
+                <TotalGraph data={totalSalesData} />
             </div>
             <div className="dashboard-item">
                 <SalesReport />
