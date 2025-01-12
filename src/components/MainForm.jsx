f609b582d0c7e6717b324a6bdf046d5d36ad3c66
boseok <fa7271@naver.com>
2025-01-12 18:49:20 +0900

    메인 화면 대시보드삭제 사이드 바에서 색칠하면 될될듯

--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -6,9 +6,6 @@ const MainForm = () => {
   return (
     <div className="main-content">
       <div className="fixed-screen bg-gray-100">
-        <div className="mt-8">
-          <h2 className="text-xl font-bold mb-4">대시보드</h2>
-        </div>
         <main className="dashboard-container">
           <DashboardGrid />
         </main>
