778662d3438e2ed8bdd8b86130dcf090471d2b51
boseok <fa7271@naver.com>
2025-01-14 11:22:45 +0900

    2025-01-14

--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import DashboardGrid from "../components/Dashboard/DashboardGrid";
+import "../styles/Mainform.css"; // 메인 폼 전용 스타일
 
 const MainForm = () => {
   return (
-    <div className="main-content">
-      <div className="fixed-screen bg-gray-100">
-        <main className="dashboard-container">
+    <div className="form-main-content">
+      <div className="form-fixed-screen bg-gray-100">
+        <main className="form-dashboard-container">
           <DashboardGrid />
         </main>
       </div>
     </div>
   );
 };
-
 export default MainForm;
