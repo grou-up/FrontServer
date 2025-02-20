09244001780f25dde8de39d7de582d0c39bcb97b
boseok <fa7271@naver.com>
2025-02-20 14:59:00 +0900

    google deploy setting

--- src/components/GoogleLoginButton.jsx
+++ src/components/GoogleLoginButton.jsx
@@ -3,8 +3,8 @@ import Button from "./Button";
 
 const GoogleLoginButton = () => {
     const handleLogin = () => {
-        console.log("구글 로그인 호출")
-        window.location.href = `http://localhost:8080/oauth2/authorization/google`;
+        // window.location.href = `http://localhost:8080/oauth2/authorization/google`;
+        window.location.href = `https://server.grouup.co.kr/oauth2/authorization/google`;
     };
 
     return (
