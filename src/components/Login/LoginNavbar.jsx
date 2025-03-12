87be632848198bfe7e4c4731569c91fb26a56ad6
boseok <fa7271@naver.com>
2025-03-12 21:50:04 +0900

    login-navbar , logo-size

--- src/components/Login/LoginNavbar.jsx
+++ src/components/Login/LoginNavbar.jsx
@@ -12,7 +12,7 @@ const LoginNavbar = () => {
                 <div className="flex space-x-2">
                     <a href="#features" className="button">기능 소개</a>
                     <a href="#pricing" className="button">요금 정책</a>
-                    <a href="#contact" className="button">진짜?가능?</a>
+                    <a href="#contact" className="button">문의</a>
                     <a href="#login" className="button">로그인</a>
                 </div>
             </div>
