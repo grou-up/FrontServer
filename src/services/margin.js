300391755b02fb03b63fba78fc26a5200e5d62ab
boseok <fa7271@naver.com>
2025-03-15 19:40:08 +0900

    메인 대시보드 빨강 지옥 탈출

--- src/services/margin.js
+++ src/services/margin.js
@@ -27,7 +27,6 @@ export const getMarginByCampaignId = async ({ startDate, endDate, campaignId }) 
 export const marginUpdatesByPeriod = async (data) => {
     try {
         const response = await apiRequest('/margin/marginUpdatesByPeriod', 'PATCH', data);
-        console.log(response)
         return response; // 성공 시 반환
     } catch (error) {
         console.error('데이터 생성 중 오류 발생:', error.message);
@@ -38,7 +37,6 @@ export const marginUpdatesByPeriod = async (data) => {
 export const getDailyMarginSummary = async ({ date }) => {
     try {
         const response = await apiRequest(`/margin/getDailyMarginSummary?date=${date}`)
-        console.log(response)
         return response;
     } catch (error) {
         throw error
@@ -47,7 +45,6 @@ export const getDailyMarginSummary = async ({ date }) => {
 export const getNetProfit = async ({ startDate, endDate, }) => {
     try {
         const response = await apiRequest(`/margin/getNetProfit?startDate=${startDate}&endDate=${endDate}`);
-        console.log(response)
         return response;
     } catch (error) {
         throw error
