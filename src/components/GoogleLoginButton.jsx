c2289cd907a9a06de51528c5da7bd42cb8fd2734
boseok <fa7271@naver.com>
2025-03-22 13:28:34 +0900

    main으로 바로

--- src/components/GoogleLoginButton.jsx
+++ src/components/GoogleLoginButton.jsx
@@ -3,7 +3,6 @@ import Button from "./Button";
 
 const GoogleLoginButton = () => {
     const handleLogin = () => {
-        console.log("구글 로그인 호출")
         window.location.href = `https://server.grouup.co.kr/oauth2/authorization/google`;
     };
 
