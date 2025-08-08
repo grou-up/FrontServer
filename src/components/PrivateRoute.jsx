a620540e156f8152117f01ddaaab5e09ca601c64
boseok <fa7271@naver.com>
2025-08-09 00:05:53 +0900

    이동 및 삭제

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -5,7 +5,6 @@ import { getToken } from "../utils/tokenManager";
 const PrivateRoute = ({ children }) => {
     // JWT 토큰을 localStorage에서 가져오기
     const token = getToken()
-    console.log(token)
     if (!token) {
         // 토큰이 없으면 로그인 페이지로 리다이렉트
         return <Navigate to="/" replace />;
