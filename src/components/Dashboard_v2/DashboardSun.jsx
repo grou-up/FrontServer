b27a50c88a0a372fd232d67a74cf81b582297ece
boseok <fa7271@naver.com>
2025-09-04 15:59:08 +0900

    없는 기능 수정

--- src/components/Dashboard_v2/DashboardSun.jsx
+++ src/components/Dashboard_v2/DashboardSun.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const DashboardSun = ({ value }) => {
     return (
         <div className="dashboard-card dashboard-sun">
-            <div className="card-label">보유중인 햇살</div>
-            <div className="card-value">0개</div>
+            <div className="card-label">개발중</div>
+            {/* <div className="card-value">0개</div> */}
         </div>
     );
 };
